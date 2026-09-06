7c0190869dcbf159d4059a847c190ea9ee2e4712
tmatthews-dotcom <tmatthews@colgate.edu>
2026-09-06 19:57:23 -0400

    Add files via upload

---
+++ CustomSearch.java
@@ -0,0 +1,90 @@
+import java.util.ArrayList;
+public class CustomSearch {
+
+    private static final boolean debug = false;
+
+    private static void debug(String output) {
+        if (debug)
+            System.out.println(output);
+    }
+
+    /**
+     * @param <T>
+     * @param list
+     * @param target
+     * @param index
+     * @return
+     */
+    private static <T extends Comparable<? super T>>
+    int checkIndex(SortedList<T> list, T target, int index) {
+        T item = list.get(index);
+        debug(String.format("%d: %s\n", index, item));
+        if (item == null) return -1;
+        return item.compareTo(target); // negative if target > item
+    }
+    
+    public static <T extends Comparable<? super T>>
+    int locateItem(SortedList<T> list, T target)
+    {
+        debug("Looking for: " + target.toString());
+
+        int comp = checkIndex(list, target, 0);
+
+        int prev = 0;
+        int current = 1;
+
+        while (comp < 0) {
+            prev = current;
+            current += 2;
+            comp = checkIndex(list, target, current);
+        }
+        
+        return binarySearch(list, target, prev, current);
+    }
+
+    private static <T extends Comparable<? super T>>
+    int binarySearch(SortedList<T> list, T target, int prev, int current) {
+        int high = current;
+        int low = prev;
+
+        while (low <= high) {
+            int mid = (high+low)/2;
+            int comp = checkIndex(list, target, mid);
+            if (comp > 0) high = mid - 1;
+            else if (comp < 0) low = mid + 1;
+            else return mid;
+        }
+
+        return -1;
+    }
+    public static void main(String[] args) {
+
+        ArrayList<Integer> a = new ArrayList<Integer>();
+        for (int x = 0; x < 800000; x += 2)
+            a.add(x);
+        SortedList<Integer> list = new SortedList<Integer>(a);
+
+
+        int position = locateItem(list, 0);
+        int queries = list.getCount();
+
+
+        System.out.println(position);
+        System.out.println(queries);
+
+        for (int x = 800000; x < 1800000; x += 2)
+            a.add(x);
+        list = new SortedList<Integer>(a);
+
+        position = locateItem(list, 15);
+        queries = list.getCount();
+
+        System.out.println(position);
+        System.out.println(queries);
+
+       
+
+
+    }
+
+}
